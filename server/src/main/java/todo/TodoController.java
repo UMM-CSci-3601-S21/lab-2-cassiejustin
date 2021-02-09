293495142150e6bd33a2653991c03659f1a9bc1c
Cassie Schultz <schu4276@morris.umn.edu>
2021-02-09 15:28:06 -0600

    Implement getTodos for todo controller
    This creates a `getTodos()` method that calls the `listTodos()` method that was created earlier in the TodoDatabase.  this also adds needed imports for the `TodoController` class to function (issue #2)

--- server/src/main/java/todo/TodoController.java
+++ server/src/main/java/todo/TodoController.java
@@ -1,5 +1,29 @@
 package todo;
 
+import org.eclipse.jetty.server.session.DatabaseAdaptor;
+
+import io.javalin.http.Context;
+import io.javalin.http.NotFoundResponse;
+
 public class TodoController {
+
+    private TodoDatabase database;
+
+    public TodoController(TodoDatabase database) {
+      this.database = database;
+    }
+
+
+     /**
+   * Get a JSON response with a list of all the users in the "database".
+   *
+   * @param ctx a Javalin HTTP context
+   */
+  public void getTodos(Context ctx) {
+    Todo[] users = database.listTodos(ctx.queryParamMap());
+    ctx.json(users);
+  }
+
+
 
 }
