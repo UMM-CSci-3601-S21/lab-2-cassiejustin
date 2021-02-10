a9bb2523ddfe8c13bc7eace2a349f0ec43e0c595
Cassie Schultz <schu4276@morris.umn.edu>
2021-02-09 23:52:55 -0600

    Refactor Filter by Status tests
    This adds a `@BeforeEach` that cuts down on some repetitive code that was within the tests.

--- server/src/test/java/FilterTodosByStatus.java
+++ server/src/test/java/FilterTodosByStatus.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
 
 import java.io.IOException;
 import java.util.Arrays;
@@ -6,8 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.javalin.http.Context;
 import todo.Todo;
 import todo.TodoDatabase;
 
@@ -17,11 +20,21 @@ import todo.TodoDatabase;
  */
 public class FilterTodosByStatus {
 
+
+  private static TodoDatabase db;
+  private Context ctx = mock(Context.class);
+  Map<String, List<String>> queryParams;
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    ctx.clearCookieStore();
+    db = new TodoDatabase("/todos.json");
+    queryParams = new HashMap<>();
+
+  }
+
   @Test
   public void listTodosByStatusComplete() throws IOException {
-    TodoDatabase db = new TodoDatabase("/todos.json");
-    Map<String, List<String>> queryParams = new HashMap<>();
-
     queryParams.put("status", Arrays.asList(new String[] { "complete" }));
     Todo[] completeTodos = db.listTodos(queryParams);
     assertEquals(143, completeTodos.length, "Incorrect number of Todos with complete Status");
@@ -29,12 +42,18 @@ public class FilterTodosByStatus {
 
   @Test
   public void listTodosByStatusIncomplete() throws IOException {
-    TodoDatabase db = new TodoDatabase("/todos.json");
-    Map<String, List<String>> queryParams = new HashMap<>();
-
     queryParams.put("status", Arrays.asList(new String[] { "incomplete" }));
     Todo[] incompleteTodos = db.listTodos(queryParams);
     assertEquals(157, incompleteTodos.length, "Incorrect number of Todos with complete Status");
+  }
+  @Test
+  public void firstTodoWithIncomplete() throws IOException {
+    queryParams.put("status", Arrays.asList(new String[] { "incomplete" }));
+    Todo[] incompleteTodos = db.listTodos(queryParams);
+    Todo firstTodo = incompleteTodos[0];
+    assertEquals(firstTodo.status, false, "Incorrect status for first Todo");
+    assertEquals(firstTodo.owner, "Blanche", "Incorrect Owner for first incomplete todo");
+    assertEquals(firstTodo._id, "58895985a22c04e761776d54", "incorrect id for first incomplete Todo");
   }
 
 }
