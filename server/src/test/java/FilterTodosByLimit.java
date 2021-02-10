a09e1ff3fed341cea1a37524fdc28fcd0ccb1b26
Cassie Schultz <schu4276@morris.umn.edu>
2021-02-10 10:31:51 -0600

    Adds testing for Todo Limit filter
    This adds a test that checks to see if the number of todos returned matches with the limit requested.

---
+++ server/src/test/java/FilterTodosByLimit.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import io.javalin.http.Context;
+import todo.Todo;
+import todo.TodoDatabase;
+
+public class FilterTodosByLimit {
+
+  private static TodoDatabase db;
+  private Context ctx = mock(Context.class);
+  Map<String, List<String>> queryParams;
+
+  @BeforeEach
+  public void setUp() throws IOException {
+    ctx.clearCookieStore();
+    db = new TodoDatabase("/todos.json");
+    queryParams = new HashMap<>();
+
+  }
+
+
+  @Test
+  public void ListTodosWithLimit() throws IOException{
+    queryParams.put("limit", Arrays.asList(new String[] {"3"}));
+    Todo[] limitedTodos = db.listTodos(queryParams);
+    assertEquals(3, limitedTodos.length, "Incorrect number of Todos");
+  }
+
+
+
+}
