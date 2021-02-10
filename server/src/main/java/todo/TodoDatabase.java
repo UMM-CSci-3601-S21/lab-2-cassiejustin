0a7b313be995aadccb806843335a5a43b39aeb98
Cassie Schultz <schu4276@morris.umn.edu>
2021-02-09 23:29:35 -0600

    Add status filter to `listTodos()`
    This adds the status filter to the listTodos method in the data base, this allows the user to specifiy a status ( complete or incomplete). This also adds a new `filterTodosByStatus()` method that does the actual filtering, and is used within `listTodos()`

--- server/src/main/java/todo/TodoDatabase.java
+++ server/src/main/java/todo/TodoDatabase.java
@@ -33,26 +33,27 @@ public class TodoDatabase {
    * @return an array of all the users matching the given criteria
    */
   public Todo[] listTodos(Map<String, List<String>> queryParams) {
-    Todo[] filteredUsers = allTodos;
+    Todo[] filteredTodos = allTodos;
 
     // Filter age if defined
-    if (queryParams.containsKey("age")) {
-      String ageParam = queryParams.get("age").get(0);
+    if (queryParams.containsKey("status")) {
+      String statusParam = queryParams.get("status").get(0);
+
       try {
-        // int targetAge = Integer.parseInt(ageParam);
-        // filteredUsers = filterUsersByAge(filteredUsers, targetAge);
+        boolean targetStatus=true;
+        if (statusParam == "complete"){
+            targetStatus = true;
+        }
+        else{
+            targetStatus = false;
+        }
+
+        filteredTodos = filterTodosByStatus(filteredTodos, targetStatus);
       } catch (NumberFormatException e) {
-        throw new BadRequestResponse("Specified age '" + ageParam + "' can't be parsed to an integer");
+        throw new BadRequestResponse("Specified status '" + statusParam + "' can't be parsed to an integer");
       }
     }
-    // Filter company if defined
-    if (queryParams.containsKey("company")) {
-      // String targetCompany = queryParams.get("company").get(0);
-      // filteredUsers = filterUsersByCompany(filteredUsers, targetCompany);
-    }
-    // Process other query parameters here...
-
-    return filteredUsers;
+    return filteredTodos;
   }
 
 
@@ -64,7 +65,12 @@ public class TodoDatabase {
    * @return the user with the given ID, or null if there is no user with that ID
    */
   public Todo getTodo(String id) {
-    return Arrays.stream(allTodos).filter(x -> x._id.equals(id)).findFirst().orElse(null);
+    return Arrays.stream(allTodos).filter(x -> x._id == (id)).findFirst().orElse(null);
+  }
+
+  public Todo[] filterTodosByStatus(Todo[] todos, boolean status){
+    return Arrays.stream(todos).filter(x -> x.status == status).toArray(Todo[]::new);
+
   }
 
 
